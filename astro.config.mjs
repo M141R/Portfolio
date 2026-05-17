93e2c927486824d1ff6826b5b76eb60f3af179d1
Mihir Kumar <mihir2726@gmail.com>
2026-05-17 12:14:43 +0530

    Added netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,15 @@ import react from "@astrojs/react";
 import markdoc from "@astrojs/markdoc";
 import keystatic from "@keystatic/astro";
 
+import netlify from "@astrojs/netlify";
+
 export default defineConfig({
   site: "https://itsmihir.me/",
   integrations: [sitemap(), react(), markdoc(), keystatic()],
+
   redirects: {
     "/sitemap": "/sitemap-index.xml",
   },
+
+  adapter: netlify(),
 });
